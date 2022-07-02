69243d01bb2bf259f320da2bf5bd624c00d8556f
stargrep <mikenyc1207@gmail.com>
2022-07-02 10:18:48 -0400

    update assignment3&project2

--- week05/mini-project-2-sol/server/web-server.py
+++ week05/mini-project-2-sol/server/web-server.py
@@ -1,12 +1,10 @@
+import pandas as pd
 from flask import Flask
 from flask import request
-from flask import json
 
 app = Flask(__name__)
 
 
-# -- DO NOT EDIT
-# sample end point for HTTP Get
 @app.route("/")
 def default():
     """
@@ -17,9 +15,6 @@ def default():
     return "FIRST PROJECT - we have " + str(len(get_client_rates())) + " clients in total."
 
 
-# sample data load function
-# This is a temporary data file - when we get to know more about database and cloud storage
-# we would not be using this kind of data storage.
 def get_client_rates():
     """
     return all the client - rate pairs we have.
@@ -29,11 +24,8 @@ def get_client_rates():
     import pandas as pd
     df = pd.read_json("client_rate.json")
     return df.to_dict()
-# -- DO NOT EDIT END
 
 
-# -- TODO: Part 1 - add an endpoint to get rate by client id
-# When query http://hostname/rate/client1 it would return the rate number for client1 - 0.2
 @app.route("/rate/<client_id>")
 def get_client_rate(client_id):
     """
@@ -42,12 +34,12 @@ def get_client_rate(client_id):
     :param client_id: str
     :return: http response
     """
-    # How to get the actual rate from client_id?
-    return client_id
-# -- TODO END: Part 1
+    rates = get_client_rates()
+    if client_id in rates:
+        return str(rates[client_id]['rate'])
+    return '0'
 
 
-# -- TODO: Part 4 - add an endpoint to add more client and rates data
 @app.route("/rate", methods=['POST'])
 def upsert_client_rate():
     """
@@ -55,27 +47,15 @@ def upsert_client_rate():
 
     :return: http response.
     """
-    # We want to update if the client exist in the client_rate.json data
-    # Or insert a new client-rate pair into client_rate.json data
-    print(request)
+    param = request.get_json()
+    client_id = param['client_id']
+    rate = param['rate']
 
-    # After getting post request - how to update json file?
+    rates = get_client_rates()
+    rates[client_id] = {'rate': rate}
+    df = pd.DataFrame.from_dict(rates)
+    df.to_json("client_rate.json")
     return request.get_json()
-
-
-def update_client_rates(client_id, rate):
-    """
-    update or insert a client_id - rate pair.
-
-    :param client_id: string, e.g. 'client1'
-    :param rate: float, e.g. 0.1
-    :return:
-    """
-    # check if exist
-    # replace or add client rate
-    # re-write the file
-    pass
-# -- TODO END: Part 4
 
 
 if __name__ == "__main__":
